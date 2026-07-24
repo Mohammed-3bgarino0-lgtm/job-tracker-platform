a756c66a53f75ad811ffa361e4519142751e5bb1
Mohammed-3bgarino0-lgtm <Mohammed-alsakran@hotmail.com>
2026-07-25 02:45:53 +0300

    Add extension bridge contract data

---
+++ apps/extension/src/lib/contract-data.js
@@ -0,0 +1,21 @@
+export const CONTRACT_DATA = Object.freeze({
+  protocol: 'qaddem.bridge.v1',
+  extensionVersion: '1.4.0',
+  messageTypes: Object.freeze({
+    ready: 'QADDEM_BRIDGE_READY',
+    request: 'QADDEM_BRIDGE_REQUEST',
+    response: 'QADDEM_BRIDGE_RESPONSE',
+    progress: 'QADDEM_BRIDGE_PROGRESS',
+  }),
+  allowedWebOrigins: Object.freeze([
+    'https://haderksa.org',
+    'https://www.haderksa.org',
+    'https://qaddemweb-production.up.railway.app',
+    'http://localhost:3000',
+  ]),
+  scanRounds: Object.freeze({
+    quick: 4,
+    balanced: 7,
+    deep: 12,
+  }),
+});
